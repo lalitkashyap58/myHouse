07b5375ee02c35d39c6f8243c2432477e8825f39
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-29 21:57:20 +0530

    signup functionality done

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,35 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const SignUp = () => {
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/signin');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div className="bg-[url('../../assets/sign.jpg')] h-[100vh] bg-contain">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-
-   <div className="p-3 max-w-lg mx-auto rounded-md bg-slate-700 bg-blur">
-    <h1 className='text-3xl text-center text-white  font-sans font-extrabold my-7'>
-      Sign Up
-    </h1>
-    <form className='flex flex-col gap-4'>
-      <input type='text' placeholder='username' className='border p-3 rounded-lg ' id='username'/>
-      <input type='text' placeholder='email' className='border p-3 rounded-lg ' id='email'/> 
-      <input type='password' placeholder='password' className='border p-3 rounded-lg ' id='password'/>  
-    <button className=' w-1/2 mx-auto bg-gradient-to-br	from bg-black font-bold text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Signup</button>
-
-    </form>
-    <div className='flex gap-2 mt-5'>
-      <p className='font-semibold text-white'>Have an Account</p>
-      <Link to={"/signin"}>
-        <span className='text-red font-serif shadow-md'>Sign in</span>
-      </Link>
-
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-   </div>
-
-   </div>
-
-
-  )
+  );
 }
-
-export default SignUp;
