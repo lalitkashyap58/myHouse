173e359adb32c4a47af2e1a754db1aef31876139
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-29 16:19:31 +0530

    we connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("db connected");
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app=express();//instance of express is created 
 
