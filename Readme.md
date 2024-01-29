ef59081c82de522928cf7a13b0e46c68977ceb39
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-29 18:10:13 +0530

    create a test api route

--- Readme.md
+++
@@ -1,2 +0,0 @@
-## connect to a database
-   
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("db connected");
@@ -10,7 +11,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app=express();//instance of express is created 
 
-app.listen(300,()=>{
+app.listen(3000,()=>{
     console.log(`server is runnning on ${3000}`)
 })
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+
+app.use('/api/user',userRouter);//send to /api/user/test/ to user.route.js
 
