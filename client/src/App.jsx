085bc9705ad07bc6265b05f865777abc0ce77d41
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-30 18:21:46 +0530

    privateRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,19 @@ import About from "./pages/About";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
-  
+import PrivateRoute from './components/PrivateRoute';
+
 const App=()=>{
   return (
     <BrowserRouter>
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
-        <Route path="/profile" element={<Profile/>} />  <Route path="/about" element={<About/>} />  <Route path="/signin" element={<Signin/>} />
+      <Route path="/about" element={<About/>} />  <Route path="/signin" element={<Signin/>} />
       <Route path="/signup" element={<SignUp/>} />
+      <Route element={<PrivateRoute/>}>
+          <Route path='/profile' element={<Profile/>} />
+        </Route>
     </Routes>
     </BrowserRouter>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,10 @@ import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
-import Signin from '../pages/Signin';
+import { useSelector } from 'react-redux';
 const Header = () => {
+  const {currentUser}=useSelector(state=>state.user.user);
+
   return (
     <header className="bg-slate-200 shadow-md h-[10vh]">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -22,9 +24,16 @@ const Header = () => {
          <Link to="/about" element={<About/>}>
          <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
          </Link>
-         <Link  to="/signin" element={<Signin/>}>
-         <li className=' text-slate-700 hover:underline cursor-pointer'>Sign in</li>
-         </Link>
+            {currentUser ? (
+              <Link to='/profile'>
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+              </Link>
+              
+            ) : (
+              <Link to='/signin'>
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              </Link>
+            )}
             
            
         </ul>
