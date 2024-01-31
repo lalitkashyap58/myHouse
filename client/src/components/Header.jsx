810920232d667f369f5d5debe94764446484f319
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-31 23:02:03 +0530

    complete project

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,11 +24,11 @@ const Header = () => {
     }
   }, [location.search]);
   return (
-    <header className="bg-slate-200 shadow-md h-[10vh]">
+    <header className=" bg-violet-50 shadow-md h-[10vh]">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
-          <span className="text-slate-500 ">Your</span>
-          <span className="text-slate-700">Place</span>
+          <span className="text-slate-500 font-serif  ">Your</span>
+          <span className="text-slate-700 font-serif">Place</span>
         </h1>
         <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center ">
           <input type="text " placeholder="Search ..." className="focus:outline-transparent bg-transparent w-24 sm:w-64" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
@@ -39,10 +39,10 @@ const Header = () => {
         </form>
         <ul className='flex gap-4 '>
             <Link to='/' element={<Home/>}>
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer font-serif font-semibold'>Home</li>
             </Link>
          <Link to="/about" element={<About/>}>
-         <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
+         <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer font-serif font-semibold'>About</li>
          </Link>
          <Link to='/profile'>
 
