ca17213c39f8a4d24c78693ebfb4a06a92d073af
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-31 21:40:59 +0530

    search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 const App=()=>{
   return (
     <BrowserRouter>
@@ -27,6 +28,7 @@ const App=()=>{
             path='/update-listing/:listingId'
             element={<UpdateListing />}
           />  
+          <Route path='/search' element={<Search />} />
     </Routes>
     </BrowserRouter>
   )
