986177e7a09336e55958241ed7a6e4718f4f8559
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-31 11:16:13 +0530

    update functioanlity

--- client/src/redux/user.Slice.js
+++ client/src/redux/user.Slice.js
@@ -1,27 +1,49 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const userSlice=createSlice({
-    name:'user',
-    initialState:{
-currentUser:null,
-error:null,
-loading:false,
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
     },
-    reducers:{
-signInStart:(start)=>{
-    start.loading=true;
-},
-signInSuccess:(state,action)=>{
-    state.currentUser=action.payload;
-    state.loading=false;
-    state.error=null;
-},
-signInFailuer:(state,action)=>{
-    state.error=action.payload;
-    state.loading=false;
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailuer: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
 
-}
-    }
-})
-export const {signInStart,signInSuccess,signInFailuer}=userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailuer,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+} = userSlice.actions;
+
 export default userSlice.reducer;
